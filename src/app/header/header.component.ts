d02859029139f82ba7c6253bdafc266aab3850ae
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-12-06 02:07:23 +0530

    ADD - post list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-header', //this name should be displayed in app root file to get header
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
