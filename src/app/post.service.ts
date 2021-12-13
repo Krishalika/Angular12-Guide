1b7262dd7af6ba2805858a71998737e2fed25ff9
Krishalika <krishalikadilanirathnayake@gmail.com>
2021-12-14 02:17:20 +0530

    integrate facilities

---
+++ src/app/post.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+
+@Injectable({ providedIn: 'root' }) //this service is available throughout the project root level
+export class PostService {
+  listOfPosts: Post[] = [
+    new Post(
+      'Nature',
+      'Nature is a British weekly scientific journal founded and based in London, England As a multidesciplinary publication, Nature features peer-reviewed reserach from a variety of academic disciplines, mainly in science and technology',
+      'https://www.stockvault.net/data/2007/03/01/99589/thumb16.jpg',
+      'test@test.com',
+      new Date()
+    ),
+    new Post(
+      'Humpi',
+      'Hampi is an ancient village in the south Indian state of Karnataka. It’s dotted with numerous ruined temple complexes from the Vijayanagara Empire. On the south bank of the River Tungabhadra is the 7th-century Hindu Virupaksha Temple, near the revived Hampi Bazaar. A carved stone chariot stands in front of the huge Vittala Temple site. Southeast of Hampi, Daroji Bear Sanctuary is home to the Indian sloth bear',
+      'https://images.newindianexpress.com/uploads/user/imagelibrary/2021/7/26/w900X450/Hampi_ruins_to_shine-.jpg',
+      'test@test.com',
+      new Date()
+    ),
+    new Post(
+      'Sigiriya',
+      'Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka. It is a site of historical and archaeological significance that is dominated by a massive column of rock around 180 metres high.',
+      'https://upload.wikimedia.org/wikipedia/commons/4/4c/Beauty_of_Sigiriya_by_Binuka.jpg',
+      'test@test.com',
+      new Date()
+    ),
+  ];
+
+  //facility1
+  getPosts() {
+    return this.listOfPosts;
+  }
+
+  //facility2
+  deletePost(index: number) {
+    this.listOfPosts.splice(index, 1);
+  }
+
+  //facility3
+  addPost(post: Post) {
+    this.listOfPosts.push(post);
+  }
+
+  //facility4
+  updatePost(index: number, post: Post) {
+    this.listOfPosts[index] = post;
+  }
+}
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,18 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
-  @Input() post?: Post;
-  constructor() { }
+  @Input() post?: Post; //Input is a decorator-> provides an additional identity
+  @Input() index:number =0;
+  memberName = 'Sam';
+
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
     console.log(this.post);
-    
   }
 
+  onDelete() {
+    this.postService.deletePost(this.index);
+  }
 }
